9cabd20271920f7180da34a7ed4455ffe6555a74
brahmcapoor <brahm@stanford.edu>
2017-09-15 16:17:46 +0800

    Added schema view

--- postAPI/urls.py
+++ postAPI/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from postAPI import views
 
-patterns = {
-    url(r'^posts/$', views.PostList.as_view()),
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'authors', views.AuthorViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^dates/$', views.PostDateList.as_view()),
-    url(r'^posts/(?P<pk>[0-9]+)/$', views.PostList.as_view()),
-    url(r'^authors/$', views.AuthorList.as_view()),
-    url(r'^authors/(?P<pk>[0-9]+)/$', views.AuthorDetail.as_view()),
-}
+]
 
-
-urlpatterns = format_suffix_patterns(patterns)
--- postAPI/views.py
+++ postAPI/views.py
@@ -1,35 +1,44 @@
 from django.contrib.auth.models import User as Author
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Post
 from .serializers import PostSerializer, PostDateSerializer, AuthorSerializer
 from .permissions import IsOwnerOrReadOnly
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+@api_view(['GET'])
+@permission_classes((IsOwnerOrReadOnly,))
+def api_root(request, format=None):
+    return Response({
+        'authors': reverse('author-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format)
+    })
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
 
 class PostDateList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDateSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly)
+                          IsOwnerOrReadOnly,)
 
-class AuthorList(generics.ListAPIView):
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Automatically provides list and details
+    """
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-
-class AuthorDetail(generics.RetrieveAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-
 
 
